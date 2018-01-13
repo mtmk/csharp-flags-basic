77768a1b188fe6e54286b3b14b4e6f0a3131ab34
Ziya Suzen <ziya@suzen.net>
2018-01-13 20:01:41 +0000

    Copy of a test for .net45

---
+++ msvs/FlagsBasic45Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FlagsBasic45Test
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return FlagsBasic.Flags.ParseAndRun(args);
+        }
+    }
+
+    class MyCmd
+    {
+        public static int Run(string argBla1, int num1, bool b1, int num2 = 42, string argFoo2 = "fourtee two", bool b2 = false)
+        {
+            Console.WriteLine($"You said arg1: {argBla1} {num1} {num2} {argFoo2} {b1} {b2}");
+            return 0;
+        }
+    }
+}
